f87dab42ef51d469741fd7206d459d53b04233ff
Brian Vu <brianvu@email.arizona.edu>
2021-09-30 20:30:57 -0700

    practicum5

---
+++ imagequiz/src/components/SignUp.js
@@ -0,0 +1,50 @@
+import * as ReactBootStrap from 'react-bootstrap'
+import Form from 'react-bootstrap/Form'
+function SignUp() {
+    return (
+        <div>
+            <ReactBootStrap.Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <ReactBootStrap.Container>
+                    <ReactBootStrap.Navbar.Brand href="#home">Image Quiz</ReactBootStrap.Navbar.Brand>
+                    <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
+                    <ReactBootStrap.Nav className="me-auto">
+                    <ReactBootStrap.Nav.Link href="#login">login</ReactBootStrap.Nav.Link>
+                    </ReactBootStrap.Nav>
+                    <ReactBootStrap.Nav>
+                    </ReactBootStrap.Nav>
+                    </ReactBootStrap.Navbar.Collapse>
+                </ReactBootStrap.Container>
+            </ReactBootStrap.Navbar>
+            
+            <h1>Sign up!</h1>
+            <Form.Floating className="mb-3">
+                <Form.Control
+                    id="floatingInputCustom"
+                    type="email"
+                    placeholder="name@example.com"
+                />
+                <label htmlFor="floatingInputCustom">Email address</label>
+            </Form.Floating>
+            <Form.Floating>
+                <Form.Control
+                type="username"
+                placeholder="Username"
+                />
+                <label htmlFor="floatingPasswordCustom">Username</label>
+            </Form.Floating>
+            <Form.Floating>
+                    <Form.Control
+                    id="floatingPasswordCustom"
+                    type="password"
+                    placeholder="Password"
+                />
+                <label htmlFor="floatingPasswordCustom">Password</label>
+            </Form.Floating>
+            
+  
+        </div>
+    );
+}
+
+export default SignUp;
